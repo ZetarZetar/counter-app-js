0985a1c4754e233fd82c9152856587235fbbe762
ZetarZetar <zss15128@gmail.com>
2023-12-04 10:47:27 +0700

    🔨 demo1 project architecture

--- backend/plugin-incdec/counterOperations.js
+++
@@ -1,9 +0,0 @@
-// Increment counter value
-export const incrementCounter = (currentValue) => {
-  return currentValue + 1;
-};
-
-// Decrement counter value
-export const decrementCounter = (currentValue) => {
-  return currentValue - 1;
-};
